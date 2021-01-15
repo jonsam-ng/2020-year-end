826f4478591f09183f63cd6df2ca94a12ee6edb1
wuqingshan <wuqingshan@xylink.com>
2021-01-16 03:26:54 +0800

    build: build static pages

---
+++ build/precache-manifest.d18ba6487e10670f25aa4f52b3a4c1d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd9916d47354143ff3e5d27b239c1da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efcd7d17e86911da2996",
+    "url": "/static/css/3.1765c934.chunk.css"
+  },
+  {
+    "revision": "dfee31ae6f163e22229c",
+    "url": "/static/css/main.e756ec91.chunk.css"
+  },
+  {
+    "revision": "096cfcd63d80712202e6",
+    "url": "/static/js/2.2fd6bed9.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/static/js/2.2fd6bed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efcd7d17e86911da2996",
+    "url": "/static/js/3.acebe8f8.chunk.js"
+  },
+  {
+    "revision": "dfee31ae6f163e22229c",
+    "url": "/static/js/main.b5a1142d.chunk.js"
+  },
+  {
+    "revision": "a09675eb08652ae87429",
+    "url": "/static/js/runtime-main.0868f96f.js"
+  },
+  {
+    "revision": "1aa4ba9418819745e0c4fcd327a5b6a1",
+    "url": "/static/media/img-lock-bg.1aa4ba94.jpg"
+  }
+]);
