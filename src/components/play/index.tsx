38bda148ae99fe58a547d191d37c45f4c1df84ac
wuqingshan <wuqingshan@xylink.com>
2021-01-17 15:00:47 +0800

    fix: set player autoplay

--- src/components/play/index.tsx
+++ src/components/play/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useRef } from "react";
 import { useStore, actionType } from "../../store";
 import { useHistory } from "react-router-dom";
 import hangoutImage from "../../assets/image/hangout.png";
@@ -9,6 +9,7 @@ import style from "./index.module.scss";
 const PlayPage: FC = () => {
   const history = useHistory();
   const { state, dispatch } = useStore();
+  const videoRef: any = useRef();
   const { pageIndex } = state;
   const { cdnUrl } = config;
 
@@ -19,17 +20,46 @@ const PlayPage: FC = () => {
   };
 
   useEffect(() => {
-    videojs("video-js");
-  }, [])
+    const player: any = videojs("video-js", {
+      controls: false,
+      autoplay: true,
+      preload: "auto",
+      loop: false,
+      sources: [
+        {
+          src: `${cdnUrl}/${pageIndex}.mp4`,
+          type: "video/mp4",
+        },
+      ],
+    });
+    function playVideo() {
+      player?.play();
+    }
+    player.ready(function() {
+      if(!player.isFullscreen()) {
+        player.isFullscreen(true);
+      }
+      playVideo();
+    });
+    document.addEventListener("WeixinJSBridgeReady", playVideo, false);
+    return () => {
+      document.removeEventListener("WeixinJSBridgeReady", playVideo, false);
+      player.dispose();
+    };
+  }, [cdnUrl, pageIndex]);
 
   return (
     <div className={style.play_container}>
       <div className={style.play_wrapper}>
         <video
           id="video-js"
-          data-setup="{}"
           className={`video-js ${style.video_player}`}
-          src={`${cdnUrl}/${pageIndex}.mp4`}
+          ref={videoRef}
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+          }}
           autoPlay={true}
           controls={false}
           onEnded={handleHangoutClick}
@@ -42,7 +72,8 @@ const PlayPage: FC = () => {
           x5-video-player-fullscreen="false"
           x5-video-orientation="portraint"
           x5-video-ignore-metadata="true"
-        />
+        >
+        </video>
       </div>
       <div className={style.hangout_box}>
         <img src={hangoutImage} alt="hangout" onClick={handleHangoutClick} />
