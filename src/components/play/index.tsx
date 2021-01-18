b863b383615d67058ed90b1f6cc37d39b8d92452
wuqingshan <wuqingshan@xylink.com>
2021-01-18 23:11:26 +0800

    fix: set attr in videoplayer

--- src/components/play/index.tsx
+++ src/components/play/index.tsx
@@ -47,10 +47,32 @@ const PlayPage: FC = () => {
     function playVideo() {
       player?.play();
     }
+    const videoPlayer:any = document.getElementById("video-js");
     player.ready(function () {
       player.setAttribute("x5-playsinline", "true");
       player.setAttribute("playsinline", "true");
       player.setAttribute("webkit-playsinline", "true");
+      player.setAttribute("x5-video-player-type", "h5");
+      player.setAttribute("x-webkit-airplay", "true");
+      player.setAttribute("x5-video-player-fullscreen", "true");
+      player.setAttribute("x5-video-orientation", "true");
+      player.setAttribute("x5-video-ignore-metadata", "true");
+      player.setAttribute(
+        "controlslist",
+        "nofullscreen nodownload noremoteplayback"
+      );
+      videoPlayer.setAttribute("x5-playsinline", "true");
+      videoPlayer.setAttribute("playsinline", "true");
+      videoPlayer.setAttribute("webkit-playsinline", "true");
+      videoPlayer.setAttribute("x5-video-videoPlayer-type", "h5");
+      videoPlayer.setAttribute("x-webkit-airplay", "true");
+      videoPlayer.setAttribute("x5-video-videoPlayer-fullscreen", "true");
+      videoPlayer.setAttribute("x5-video-orientation", "true");
+      videoPlayer.setAttribute("x5-video-ignore-metadata", "true");
+      videoPlayer.setAttribute(
+        "controlslist",
+        "nofullscreen nodownload noremoteplayback"
+      );
       playVideo();
     });
     document.addEventListener("WeixinJSBridgeReady", playVideo, false);
